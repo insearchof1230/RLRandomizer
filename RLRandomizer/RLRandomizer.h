331dbcafbd58a503819e0cda9b769b1d479cf989
Adrian Crutchfield <insearchof@Adrians-MacBook-Pro.local>
2013-10-29 18:54:46 -0400

    Initial Commit

---
+++ RLRandomizer/RLRandomizer.h
@@ -0,0 +1,13 @@
+//
+//  RLRandomizer.h
+//  RLRandomizer
+//
+//  Created by Adrian Crutchfield on 10/29/13.
+//  Copyright (c) 2013 Adrian Crutchfield. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RLRandomizer : NSObject
+
+@end
---
+++ RLRandomizer/RLRandomizer.m
@@ -0,0 +1,13 @@
+//
+//  RLRandomizer.m
+//  RLRandomizer
+//
+//  Created by Adrian Crutchfield on 10/29/13.
+//  Copyright (c) 2013 Adrian Crutchfield. All rights reserved.
+//
+
+#import "RLRandomizer.h"
+
+@implementation RLRandomizer
+
+@end
